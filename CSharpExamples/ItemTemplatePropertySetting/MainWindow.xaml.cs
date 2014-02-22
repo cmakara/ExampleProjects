466a0d6718a445457e6ec52cd8f2c12c3cfae276
cSE- <csaba.makara@gmail.com>
2014-02-22 23:51:48 +0100

    - C# examples solution created - ItemTemplatePropertySetting project created

---
+++ CSharpExamples/ItemTemplatePropertySetting/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace ItemTemplatePropertySetting
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var firstEntity = new MyEntity() { Text = "parentText", Type = "parentType" };
+            var secondEntity = new MyEntity() { Text = "childText", Type = "childType" };
+            firstEntity.Children.Add(secondEntity);
+            treeView.Items.Add(firstEntity);
+        }
+    }
+}
---
+++ CSharpExamples/ItemTemplatePropertySetting/MyEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemTemplatePropertySetting
+{
+    public class MyEntity
+    {
+        private readonly ICollection<MyEntity> _Children = new ObservableCollection<MyEntity>();
+
+        public string Text { get; set; }
+
+        public string Type { get; set; }
+
+        public ICollection<MyEntity> Children
+        {
+            get
+            {
+                return _Children;
+            }
+        }
+
+    }
+}
